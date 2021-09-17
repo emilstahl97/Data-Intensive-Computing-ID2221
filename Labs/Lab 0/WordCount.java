ef20424335023f674c8d115cf3cb0f933ba7b032
Emil Ståhl <emil.stahl@icloud.com>
2021-09-17 14:33:30 +0200

    save

--- Labs/Lab 0/WordCount.java
+++ Labs/Lab 0/WordCount.java
@@ -26,8 +26,8 @@ public class WordCount {
 
   public static class IntSumReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
     private IntWritable result = new IntWritable();
-    public void reduce(Text key, Iterable<IntWritable> values, Context context)
-    throws IOException, InterruptedException {
+    
+    public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
       int sum = 0;
       for (IntWritable val : values) {
         sum += val.get();
