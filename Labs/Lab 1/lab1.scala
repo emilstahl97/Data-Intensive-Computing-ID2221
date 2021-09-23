6b9a7d1b5145333eaf3ff566d85b5add3ec8a465
lelti <selamfsha3@gmail.com>
2021-09-23 22:19:02 +0200

    Change field name

--- Labs/Lab 1/lab1.scala
+++ Labs/Lab 1/lab1.scala
@@ -1,7 +1,7 @@
 val pagecounts = sc.textFile("/FileStore/tables/pagecounts_20160101_000000_parsed-1.out")
 
 // *1. Create a case class called Log using the four field names of the dataset.
-case class Log(field1: String, field2: String, field3: String, field4: String)
+case class Log(projectName: String, pageTitle: String, numberOfRequests: String, pageSize: String)
 
 // *2. Create a function that takes a string, split it by white space and converts it into a log object.
 // *3. Create a function that takes an RDD[String] and returns an RDD[Log]
@@ -16,44 +16,44 @@ println("Total number of records in the dataset is " + totalNumbeOfRecords)
 
 // 3. Compute the min, max, and average page size.
 val maxPages = pageCountsCollection.reduce((acc,value) => { 
-  if(acc.field4 < value.field4) value else acc})
-println("The maximum page size is " + maxPages.field4)
+  if(acc.pageSize < value.pageSize) value else acc})
+println("The maximum page size is " + maxPages.pageSize)
 
 val minPages = pageCountsCollection.reduce((acc,value) => { 
-  if(acc.field4 > value.field4) value else acc})
-println("The minimum page size is " + minPages.field4)
+  if(acc.pageSize > value.pageSize) value else acc})
+println("The minimum page size is " + minPages.pageSize)
 
-val average = pageCountsCollection.map(_.field4.toLong).sum/totalNumbeOfRecords.toLong
+val average = pageCountsCollection.map(_.pageSize.toLong).sum/totalNumbeOfRecords.toLong
 println("The average page size is " + average)
 
 // 4. Determine the record(s) with the largest page size. If multiple records have the same size, list all of them.
-val largePageSize = pageCountsCollection.filter(page => page.field4 == maxPages.field4)
+val largePageSize = pageCountsCollection.filter(page => page.pageSize == maxPages.pageSize)
 println("List of records with largest page size: ")
 largePageSize.collect.foreach(println)
 
 // 5. Determine the record with the largest page size again. But now, pick the most popular.
 val largePageSizeAndPopular = largePageSize.reduce((acc,value) => { 
-  if(acc.field3 < value.field3) value else acc})
+  if(acc.numberOfRequests < value.numberOfRequests) value else acc})
 println("Records with largest page size and that are the most popular " + largePageSizeAndPopular)
 
 // 6. Determine the record(s) with the largest page title. If multiple titles have the same length, list all of them.
 val maxTitleLength = pageCountsCollection.reduce((acc,value) => { 
-  if(acc.field2.length() < value.field2.length()) value else acc})
+  if(acc.pageTitle.length() < value.pageTitle.length()) value else acc})
 
-val largePageTitle = pageCountsCollection.filter(page => page.field2.length() == maxTitleLength.field2.length())
+val largePageTitle = pageCountsCollection.filter(page => page.pageTitle.length() == maxTitleLength.pageTitle.length())
 
 println("List of records with longest page title: ")
 largePageTitle.collect.foreach(println)
 
 //7. Use the results of Question 3, and create a new RDD with the records that have greater page size than the average.
-val pagesWithAboveAverageSizeCollection = pageCountsCollection.filter(page => page.field4.toLong > average.toLong)
+val pagesWithAboveAverageSizeCollection = pageCountsCollection.filter(page => page.pageSize.toLong > average.toLong)
 // pagesWithAboveAverageSizeCollection.collect.foreach(println)
 
 val pagesWithAboveAverageSizeRDD = sc.parallelize(Seq(pagesWithAboveAverageSizeCollection))
 // pagesWithAboveAverageSizeRDD.collect.foreach(println)
 
 // 8. Compute the total number of pageviews for each project (as the schema shows, the first field of each record contains the project code).
-val projectViewCollection = pageCountsCollection.map(page => (page.field1, page.field3.toInt))
+val projectViewCollection = pageCountsCollection.map(page => (page.projectName, page.numberOfRequests.toInt))
 // projectViewCollection.take(15).foreach(println)
 val projectTotalViews = projectViewCollection.reduceByKey((x,y)=>x+y)
 
@@ -63,19 +63,19 @@ println("Top ten popular pages with high total views: ")
 projectTotalViewsSorted.take(10).foreach(println)
 
 // 10. Determine the number of page titles that start with the article "The". How many of those page titles are not part of the English project (Pages that are part of the English project have "en" as the first field)?
-var pagesThatStartWithThe = pageCountsCollection.filter(page => page.field2.startsWith("The"))
+var pagesThatStartWithThe = pageCountsCollection.filter(page => page.pageTitle.startsWith("The"))
 // var pagesThatStartWithTheCount = pagesThatStartWithThe.count()
 // pagesThatStartWithThe.take(3).foreach(println)
 // println(pagesThatStartWithThe)
 println("The total number of pages that start with 'The' are " + pagesThatStartWithThe.count())
 
-var nonEnglishPagesThatStartWithThe = pagesThatStartWithThe.filter(page => !page.field1.equals("en"))
+var nonEnglishPagesThatStartWithThe = pagesThatStartWithThe.filter(page => !page.projectName.equals("en"))
 // var nonEnglishPagesThatStartWithTheCount = nonEnglishPagesThatStartWithThe.count()
 // pagesThatStartWithThe.take(3).foreach(println)
 println("The total number of pages that start with 'The' and that are not part of English project is : " + nonEnglishPagesThatStartWithThe.count())
 
 // 11. Determine the percentage of pages that have only received a single page view in this one hour of log data.
-val pagesWithSingleView = pageCountsCollection.filter(page => page.field3.toInt == 1).count()
+val pagesWithSingleView = pageCountsCollection.filter(page => page.numberOfRequests.toInt == 1).count()
 // println("pagesWithSingleView" + pagesWithSingleView)
 val pagesWithSingleViewPercentage: Double = (pagesWithSingleView.toDouble/totalNumbeOfRecords.toDouble)*100
 println("Pages with single view in percentage " + pagesWithSingleViewPercentage + "%")
@@ -83,7 +83,7 @@ println("Pages with single view in percentage " + pagesWithSingleViewPercentage 
 
 // 12. Determine the number of unique terms appearing in the page titles. Note that in page titles, terms are delimited by "_" instead of a white space. You can use any number of normalization steps (e.g., lowercasing, removal of non-alphanumeric characters).
 // Get page terms from titles by chaning to lowercase and replace non-alpahnumeric characters with _
-val pageTerms = pageCountsCollection.map(page => page.field2.toLowerCase().replaceAll("[^a-zA-Z0-9]", "_"))
+val pageTerms = pageCountsCollection.map(page => page.pageTitle.toLowerCase().replaceAll("[^a-zA-Z0-9]", "_"))
 // Split by _
 val pageTermsSplitted = pageTerms.flatMap(l => l.split("_"))
 // As there are some empty string(""), remove those.
