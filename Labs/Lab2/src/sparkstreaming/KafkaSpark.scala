809eafac40d4c3deb277e3c757d1de2b63524a31
Emil Ståhl <emil.stahl@icloud.com>
2021-10-05 18:52:45 +0200

    think reading from kafka works

--- Labs/Lab2/src/sparkstreaming/KafkaSpark.scala
+++ Labs/Lab2/src/sparkstreaming/KafkaSpark.scala
@@ -6,45 +6,58 @@ import org.apache.kafka.common.serialization.StringDeserializer
 import org.apache.spark.streaming.kafka._
 import kafka.serializer.{DefaultDecoder, StringDecoder}
 import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
 import org.apache.spark.streaming._
 import org.apache.spark.streaming.kafka._
 import org.apache.spark.storage.StorageLevel
 import java.util.{Date, Properties}
 import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord, ProducerConfig}
 import scala.util.Random
+import org.apache.spark.sql.SparkSession
+
 
 object KafkaSpark {
   def main(args: Array[String]) {
     // make a connection to Kafka and read (key, value) pairs from it
-
-    val kafkaConf = Map(
-      "metadata.broker.list" -> "localhost:9092",
-      "zookeeper.connect" -> "localhost:2181",
-      "group.id" -> "spark-streaming-consumer",
-      "zookeeper.connection.timeout.ms" -> "1000")
-      
-    //val sc = spark.sparkContext
-    val ssc = new StreamingContext(sc, Seconds(1))
     
-    // Create direct kafka stream with brokers and topics
-    val topics = Set("avg")
-    val kafkaParams = Map[String, String]("metadata.broker.list" -> "localhost:9092")
-    val kafkaStream = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](ssc, kafkaConf, topics)
 
-    // print words
+    //create a spark context
+    val conf = new SparkConf().setAppName("KafkaSpark").setMaster("local[2]")
+    val sc = new SparkContext(conf)
+    val ssc = new StreamingContext(sc, Seconds(1))
+
+    //create spark session
+
+    val spark = SparkSession.builder.appName("KafkaSpark").getOrCreate()
+
+
+
+    // Create direct kafka stream with brokers and topics
+    val topicsSet = Set("avg")
+    val kafkaParams = Map[String, String]("metadata.broker.list" -> "localhost:9092")
+    val kafkaStream = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](ssc, kafkaParams, topicsSet)
+
+    // print kafka stream to terminal 
     kafkaStream.foreachRDD(rdd => {
-      val words = rdd.map(_._2)
-      words.foreach(println)
+      if (!rdd.isEmpty()) {
+        val lines = rdd.map(_._2)
+        lines.foreach(println)
+      }
     })
 
+    ssc.start()
+    ssc.awaitTermination()
+
+
+/*
     // measure the average value for each key in a stateful manner
-    /*
     def mappingFunc(key: String, value: Option[Double], state: State[Double]): (String, Double) = {
+	<FILL IN>
     }
     val stateDstream = pairs.mapWithState(<FILL IN>)
 
     ssc.start()
     ssc.awaitTermination()
-    */
+  */
   }
 }
