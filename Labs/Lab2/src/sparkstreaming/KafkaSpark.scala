a4acf5d97d65497b5eca41c8901b5d5896f209bd
Emil Ståhl <emil.stahl@icloud.com>
2021-10-07 00:33:08 +0200

    Update KafkaSpark.scala

--- Labs/Lab2/src/sparkstreaming/KafkaSpark.scala
+++ Labs/Lab2/src/sparkstreaming/KafkaSpark.scala
@@ -32,38 +32,21 @@ object KafkaSpark {
 
     val spark = SparkSession.builder.appName("KafkaSpark").getOrCreate()
 
-
-
     // Create direct kafka stream with brokers and topics
     val topicsSet = Set("avg")
     val kafkaParams = Map[String, String]("metadata.broker.list" -> "localhost:9092")
     val kafkaStream = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](ssc, kafkaParams, topicsSet)
-
-    // print kafka stream to terminal 
-    kafkaStream.foreachRDD(rdd => {
-      if (!rdd.isEmpty()) {
-        val lines = rdd.map(_._2)
-        lines.foreach(println)
-      }
-    })
     
 
     val value = kafkaStream.map{case (key, value) => value.split(',')}
     val pairs = value.map(record => (record(1), record(2).toDouble))
 
-    // calculate the average value for each key in a stateful manner with mapWithState
-    def mappingFunc(key: String, value: Option[Double], state: State[Double]): (String, Double) = {
-      val sum = value.getOrElse(0.0) + state.getOption.getOrElse(0.0)
-      val avg = sum / 2
-      state.update(avg)
-      (key, avg)
-    }
+    // print out value and pairs
+    pairs.print()
+    value.print()
 
-    // call mappingFunc with pairs and mapwithstate
-    val stateDstream = pairs.mapWithState(StateSpec.function(mappingFunc _))
-
-    // print the stateDstream to terminal
-    stateDstream.print()
+    // compute average
+    
 
 
     ssc.start()
