45ee3daead6cb965e85670983fcc884c3e42c0ef
lelti <selamfsha3@gmail.com>
2021-10-07 23:55:46 +0200

    Started working on task2

--- Labs/Lab2/src/sparkstreaming/task2/KafkaSpark.scala
+++ Labs/Lab2/src/sparkstreaming/task2/KafkaSpark.scala
@@ -13,41 +13,26 @@ import org.apache.spark.storage.StorageLevel
 import java.util.{Date, Properties}
 import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord, ProducerConfig}
 import scala.util.Random
+import org.apache.spark.sql.functions._
 
 object KafkaSpark {
   def main(args: Array[String]) {
     // make a connection to Kafka and read (key, value) pairs from it
     val sparkConf = new SparkConf().setMaster("local[2]").setAppName("KafkaSpark")
-    val ssc = new StreamingContext(sparkConf, Seconds(10))
-    ssc.checkpoint("./checkpoints")
+    val spark = SparkSession.builder.appName("KafkaSpark").config("spark.master", "local").getOrCreate()
+    val sc = spark.sparkContext
 
-    val spark = SparkSession.builder.appName("KafkaSpark").getOrCreate()
-    spark.sparkContext.setLogLevel("ERROR")
+    val df = spark
+    .readStream
+    .format("kafka")
+    .option("kafka.bootstrap.servers", "localhost:9092")
+    .option("subscribe", "avg")
+    .load()
 
-    val topics = Set("avg")
-  
-   // read from kafka with spark structured streaming
-    val kafkaParams = Map[String, String]("metadata.broker.list" -> "localhost:9092")
-    val messages = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](
-      ssc, kafkaParams, topics)
-      
-    // calculate average value of key with spark structured streaming
-    val avg = messages.map(x => x._2.toInt).reduce(_+_)
-    avg.foreachRDD(rdd => {
-      val avg = rdd.reduce(_+_) / rdd.count()
-      println(s"Average value of key is $avg")
-    })
+    import spark.implicits._
+    df.selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)")
+      .as[(String, String)]
 
-    // calculate average value of key with spark sql
-    val df = spark.read.json(messages.map(x => x._2))
-    df.createOrReplaceTempView("avg")
-    val avg2 = spark.sql("select avg(value) from avg")
-    avg2.show()
-
-    
-
-
-    ssc.start()
-    ssc.awaitTermination()
+    spark.close
   }
 }
