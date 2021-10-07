e3c0946a00dc1ceb28fa09033f022589c4cad3d7
Emil Ståhl <emil.stahl@icloud.com>
2021-10-07 15:42:45 +0200

    Update KafkaSpark.scala

--- Labs/Lab2/src/sparkstreaming/KafkaSpark.scala
+++ Labs/Lab2/src/sparkstreaming/KafkaSpark.scala
@@ -5,37 +5,52 @@ import org.apache.kafka.clients.consumer.ConsumerConfig
 import org.apache.kafka.common.serialization.StringDeserializer
 import org.apache.spark.streaming.kafka._
 import kafka.serializer.{DefaultDecoder, StringDecoder}
+import org.apache.spark.sql.SparkSession
 import org.apache.spark.SparkConf
-import org.apache.spark.SparkContext
 import org.apache.spark.streaming._
 import org.apache.spark.streaming.kafka._
 import org.apache.spark.storage.StorageLevel
 import java.util.{Date, Properties}
 import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord, ProducerConfig}
 import scala.util.Random
-import org.apache.spark.sql.SparkSession
-
 
 object KafkaSpark {
   def main(args: Array[String]) {
-    val conf = new SparkConf().setMaster("local[2]").setAppName("KafkaSpark") // local[2] = deploy locally, need 2 threads to read and process data
-    val ssc = new StreamingContext(conf, Seconds(10)) // Window of 1 sec (not needed for direct stream??)
-    ssc.checkpoint("./checkpoints")
+    // make a connection to Kafka and read (key, value) pairs from it
+    val sparkConf = new SparkConf().setMaster("local[2]").setAppName("KafkaSpark")
+    val ssc = new StreamingContext(sparkConf, Seconds(10))
+    ssc.checkpoint("/home/cuong/projects/id2221-labs/temp")
+
+    val spark = SparkSession.builder.appName("KafkaSpark").getOrCreate()
+    spark.sparkContext.setLogLevel("ERROR")
+
     val kafkaConf = Map(
       "metadata.broker.list" -> "localhost:9092",
       "zookeeper.connect" -> "localhost:2181",
       "group.id" -> "kafka-spark-streaming",
-      "zookeeper.connection.timeout.ms" -> "1000")
+      "zookeeper.connection.timeout.ms" -> "1000"
+    )
     val topics = Set("avg")
-    // make a connection to Kafka and read (key, value) pairs from it
-    val kafkaStream = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](
+  
+    val messages = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](
       ssc, kafkaConf, topics
     )
-
-    // Extract (key, value) pairs
-    val values = kafkaStream.map(x => x._2.split(","))
+    val values = messages.map(x => x._2.split(","))
     val pairs = values.map(x => (x(0), x(1).toDouble))
     pairs.print()
+
+    // measure the average value for each key in a stateful manner
+    def mappingFunc(key: String, value: Option[Double], state: State[Double]): (String, Double) = {
+      val sum = value.getOrElse(0.0) + state.getOption().getOrElse(0.0)
+      val avg = sum / 2
+      state.update(avg)
+      (key, avg)
+    }
+    val stateDstream = pairs.mapWithState(StateSpec.function(mappingFunc _))
+    stateDstream.foreachRDD(rdd => {
+      val values = rdd.map(x => x).collect()
+      values.foreach(println)
+    })
 
     ssc.start()
     ssc.awaitTermination()
