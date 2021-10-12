95cdf82174910099269a8d0fd96f821b83d8f392
Emil Ståhl <emil.stahl@icloud.com>
2021-10-12 18:12:58 +0200

    load-script

--- Project/src/kafka/load-data.sh
+++ Project/src/kafka/load-data.sh
@@ -15,3 +15,15 @@ unzip ../../Wiki-pageviews/2016/August/pagecounts-20160802-170000.zip -d ../../W
 
 # read into kafka
 
+bash read-into-kafka.sh 2015-jul-1 ../../Wiki-pageviews/2015/July/pagecounts-20150701-170000
+bash read-into-kafka.sh 2015-jul-2 ../../Wiki-pageviews/2015/July/pagecounts-20150702-170000
+
+bash read-into-kafka.sh 2015-aug-1 ../../Wiki-pageviews/2015/Aug/pagecounts-20150801-170000
+bash read-into-kafka.sh 2015-aug-2 ../../Wiki-pageviews/2015/Aug/pagecounts-20150802-170000
+
+bash read-into-kafka.sh 2016-jul-1 ../../Wiki-pageviews/2016/July/pagecounts-20160701-170000
+bash read-into-kafka.sh 2016-jul-2 ../../Wiki-pageviews/2016/July/pagecounts-20160702-170000
+
+bash read-into-kafka.sh 2016-aug-1 ../../Wiki-pageviews/2016/Aug/pagecounts-20160801-170000
+bash read-into-kafka.sh 2016-aug-2 ../../Wiki-pageviews/2016/Aug/pagecounts-20160802-170000
+
