66102df12df03100f209b627b2a61f7341d8fe58
Emil Ståhl <emil.stahl@icloud.com>
2021-10-24 21:25:54 +0200

    Added bot count

--- Project/src/consumer/src/main/scala/com/id2221/recentchanges/consumer/AnalyticsConsumer.scala
+++ Project/src/consumer/src/main/scala/com/id2221/recentchanges/consumer/AnalyticsConsumer.scala
@@ -12,7 +12,6 @@ object AnalyticsConsumer extends App with LazyLogging {
 
   val appName: String = "analytics-consumer-example"
 
-  
   val spark: SparkSession = SparkSession.builder()
     .appName(appName)
     .config("spark.driver.memory", "5g")
@@ -21,60 +20,74 @@ object AnalyticsConsumer extends App with LazyLogging {
   
   spark.sparkContext.setLogLevel("WARN")
   import spark.implicits._ 
-
+  
   val inputStream = spark
-       .readStream.format("kafka")
-       .option("kafka.bootstrap.servers", "kafka:9092")
-       .option("auto.offset.reset", "latest")
-       .option("value.deserializer", "StringDeserializer")
-       .option("subscribe", "wikiflow-topic")
-       .load
+    .readStream.format("kafka")
+    .option("kafka.bootstrap.servers", "kafka:9092")
+    .option("auto.offset.reset", "latest")
+    .option("value.deserializer", "StringDeserializer")
+    .option("subscribe", "wikiflow-topic")
+    .load
 
   val schema = StructType(
-       List(
-         StructField("title", StringType, true),
-         StructField("user", StringType, true),
-         StructField("bot", BooleanType, true),
-         StructField("timestamp", TimestampType, true),
-         StructField("id", IntegerType, true)
-       )
-     )
+    List(
+      StructField("title", StringType, true),
+      StructField("user", StringType, true),
+      StructField("bot", BooleanType, true),
+      StructField("timestamp", TimestampType, true),
+      StructField("id", IntegerType, true)
+    )
+  )
 
-  val initial = inputStream.selectExpr("CAST(value AS STRING)").toDF("value")
-  initial.printSchema()
+  val value = inputStream.selectExpr("CAST(value AS STRING)").toDF("value")
+  value.printSchema()
 
-  var aggregation = initial.select(from_json($"value", schema))
-  aggregation.printSchema()
+  var valueJson = value.select(from_json($"value", schema))
+  valueJson.printSchema()
 
-  aggregation = initial.select(from_json($"value", schema).alias("tmp")).select("tmp.*")
-  aggregation.printSchema()
+  valueJson = value.select(from_json($"value", schema).alias("tmp")).select("tmp.*")
+  valueJson.printSchema()
 
   // count the number of edits per user
-  //val editsPerUser = aggregation.groupBy("user").count()
+  //val editsPerUser = valueJson.groupBy("user").count()
 
-    val changesPerUser = aggregation
-       .withWatermark("timestamp", "1 minutes")
-       .groupBy(window($"timestamp", "1 minute", "1 minute"), $"user", $"bot").count()
+  val changesPerUser = valueJson
+    .withWatermark("timestamp", "1 minutes")
+    .groupBy(window($"timestamp", "1 minute", "1 minute"), $"user", $"bot").count()
 
-  val numberOfArticles = aggregation
-       .withWatermark("timestamp", "1 minutes")
-       .groupBy(window($"timestamp", "1 minute", "1 minute"), $"window").count()
+  val numberOfArticles = valueJson
+    .withWatermark("timestamp", "1 minutes")
+    .groupBy(window($"timestamp", "1 minute", "1 minute"), $"window").count()
+
+  val botsCount = valueJson
+    .groupBy(window($"timestamp", "1 minute", "1 minute"))
+    .agg(
+      sum(when($"bot" === true, lit(1))).as("is_bots"),
+      sum(when($"bot" === false, lit(1))).as("non_bots")
+    )
 
   val numberOfArticlesToConsole = numberOfArticles
-  .writeStream
-  .outputMode("complete")
-  .option("truncate", false)
-  .format("console")
-  .start()  
-
+    .writeStream
+    .outputMode("complete")
+    .option("truncate", false)
+    .format("console")
+    .start()  
 
   val changesPerUserToConsole = changesPerUser
-  .writeStream
-  .outputMode("complete")
-  .option("truncate", false)
-  .format("console")
-  .start()
-  
-spark.streams.awaitAnyTermination()
+    .writeStream
+    .outputMode("complete")
+    .option("truncate", false)
+    .format("console")
+    .start()
 
+  val botsCountToConsole = botsCount
+    .writeStream
+    .outputMode("complete")
+    .option("truncate", false)
+    .format("console")
+    .start()    
+  
+  spark.streams.awaitAnyTermination()
+
+  
 }
